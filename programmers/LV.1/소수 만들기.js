272d4981b54f2483c8230e83ddbf651ce7c60eb3
Tory <88531407+1two13@users.noreply.github.com>
2023-04-21 20:43:04 +0900

    Create 소수 만들기.js

---
+++ programmers/LV.1/소수 만들기.js
@@ -0,0 +1,25 @@
+//TODO 재귀로 푸는 방법
+
+// 소수 판별 함수
+function isPrime(num) {
+  if (num % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) answer++;
+      }
+    }
+  }
+
+  return answer;
+}
