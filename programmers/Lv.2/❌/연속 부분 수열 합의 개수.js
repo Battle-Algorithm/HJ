d47abe5cbf6426c9dab9da235d701aa30581b2f9
Tory <88531407+1two13@users.noreply.github.com>
2023-05-12 10:04:07 +0900

    Update 연속 부분 수열 합의 개수.js

---
+++ programmers/Lv.2/❌/연속 부분 수열 합의 개수.js
@@ -0,0 +1,15 @@
+function solution(elements) {
+  //* concat을 사용해서 모든 경우의 수를 미리 만들기
+  const circular = elements.concat(elements);
+  const set = new Set();
+
+  for (let i = 0; i < elements.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < elements.length; j++) {
+      sum += circular[i + j];
+      set.add(sum);
+    }
+  }
+
+  return set.size;
+}
