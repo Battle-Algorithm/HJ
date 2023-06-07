1e98309d779f289a480c60ff9a82e37b26ab3733
Tory <88531407+1two13@users.noreply.github.com>
2023-06-07 21:07:33 +0900

    Create 별 찍기 - 2.js

---
+++ baekjoon/구현/별 찍기 - 2.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+
+for (let i = 1; i <= N; i++) {
+  let star = '';
+  for (let j = 1; j <= N - i; j++) {
+    star += ' ';
+  }
+  for (let k = 1; k <= i; k++) {
+    star += '*';
+  }
+  console.log(star);
+}
