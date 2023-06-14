5f7a56053cd1c7845d328ea7d0728454f569ea09
Tory <88531407+1two13@users.noreply.github.com>
2023-06-14 16:19:55 +0900

    Update 명령 프롬프트.js

--- baekjoon/구현/명령 프롬프트.js
+++ baekjoon/구현/명령 프롬프트.js
@@ -12,3 +12,26 @@ for (let i = 0; i < n; i++) {
 }
 
 console.log(arr.join(''));
+
+// 다른 코드
+const [, ...data] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const length = data[0].length;
+let answer = '';
+
+for (let i = 0; i < length; i++) {
+  let character = null;
+
+  for (let c of data.map((str) => str[i])) {
+    if (character !== null && character !== c) {
+      character = '?';
+      break;
+    }
+
+    character = c;
+  }
+
+  answer += character;
+}
+
+console.log(answer);
