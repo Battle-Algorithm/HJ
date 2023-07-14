f3a12cf6cd53116a1d564375a2303c41bf07c6e8
Tory <88531407+1two13@users.noreply.github.com>
2023-07-14 12:55:52 +0900

    Update 트럭.js

--- baekjoon/큐/❌/트럭.js
+++ baekjoon/큐/❌/트럭.js
@@ -3,23 +3,24 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 const queue = [];
 // 트럭수, 다리 길이, 다리 최대하중
-let [n, w, l] = input[0].split(' ').map(Number);
+let [n, l, w] = input[0].split(' ').map(Number);
 // i번째 트럭 무게
 let truckWeight = input[1].split(' ').map(Number);
 let answer = 0;
 let sum = 0;
 
 for (let i = 0; i < n; i++) {
+  //* while문 어떤 조건으로..? => 모르면 일단 true
   while (true) {
     // queue의 길이가 다리 길이랑 같을 때
-    if (queue.length === w) {
+    if (queue.length === l) {
       // sum - queue에서 가장 먼저 들어온 값
       sum -= queue[0];
       // queue에 가장 먼저 추가했던 트럭 무게 제거
       queue.shift();
     }
-    // 다리 최대하중보다 작을 때 while문 빠져나오기
-    if (sum + truckWeight[i] <= l) break;
+    //* 다리 최대하중보다 작을 때 while문 빠져나오기
+    if (sum + truckWeight[i] <= w) break;
 
     // 다리 최대하중보다 클 때 => 0 push, while문 돌기
     queue.push(0);
@@ -30,4 +31,4 @@ for (let i = 0; i < n; i++) {
   sum += truckWeight[i];
   answer++;
 }
-console.log((answer += w));
+console.log((answer += l));
